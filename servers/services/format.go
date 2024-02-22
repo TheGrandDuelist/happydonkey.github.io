c345750b122b981e6be81bcdbc4cc067c8dc5d7c
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-02-22 09:57:49 +0800

    fix bugs

--- servers/services/format.go
+++ servers/services/format.go
@@ -4,6 +4,11 @@ import (
 	"github.com/ybkuroki/go-webapp-sample/container"
 	"github.com/ybkuroki/go-webapp-sample/model"
 )
+// NewFormatService is constructor.
+func NewFormatService(container container.Container) FormatService {
+	return &formatService{container: container}
+}
+
 
 // FormatService is a service for managing master data such as format and category.
 type FormatService interface {
@@ -14,16 +19,12 @@ type formatService struct {
 	container container.Container
 }
 
-// NewFormatService is constructor.
-func NewFormatService(container container.Container) FormatService {
-	return &formatService{container: container}
-}
-
 // FindAllFormats returns the list of all formats.
 func (m *formatService) FindAllFormats() *[]model.Format {
+	
+	result, err := format.FindAll(rep)
 	rep := m.container.GetRepository()
 	format := model.Format{}
-	result, err := format.FindAll(rep)
 	if err != nil {
 		m.container.GetLogger().GetZapLogger().Errorf(err.Error())
 		return nil
