daca16d1a929b35fd636f891a68f7b1b8aafee3a
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-03-07 15:55:58 +0800

    修改测试文件

---
+++ servers/controllers/category_test.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ybkuroki/go-webapp-sample/container"
+	"github.com/ybkuroki/go-webapp-sample/service"
+)
+
+// CategoryController is a controller for managing category data.
+type CategorytController interface {
+	GetCategoryList(c echo.Context) error
+}
+
+type categorytController struct {
+	container container.Container
+	service   service.CategoryService
+}
+
+// NewCategoryController is constructor.
+func NewCategoryController(container container.Container) CategoryController {
+	return &categoryController{container: container, service: service.NewCategoryService(container)}
+}
+
+// NewCategoryController is constructor.
+func NewCategoryController(container container.Container) CategoryController {
+	return &categoryController{container: container, service: service.NewCategoryService(container)}
+}
+
+
+// GetCategoryList returns the list of all categories.
+// @Summary Get a category list
+// @Description Get a category list
+// @Tags Categories
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} model.Category "Success to fetch a category list."
+// @Failure 401 {string} false "Failed to the authentication."
+// @Router /categories [get]
+func (controller *categoryController) GetCategoryList(c echo.Context) error {
+	return c.JSON(http.StatusOK, controller.service.FindAllCategories())
+}
+
+// NewCategoryController is constructor.
+func NewCategoryController(container container.Container) CategoryController {
+	return &categoryController{container: container, service: service.NewCategoryService(container)}
+}
