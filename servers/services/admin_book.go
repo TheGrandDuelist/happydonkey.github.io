0e43a7c335b21c3185136b06d62021c7eba867da
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-03-15 16:40:04 +0800

    修改查询

--- servers/services/admin_book.go
+++ servers/services/admin_book.go
@@ -39,3 +39,49 @@ func (a *accountService) AuthenticateByUsernameAndPassword(username string, pass
 
 	return true, result
 }
+
+func (a *accountService) AuthenticateByUsernameAndPwd(username string, password string) (bool, *model.Account) {
+	result, err := account.FindByName(rep, username)
+	rep := a.container.GetRepository()
+	logger := a.container.GetLogger()
+	account := model.Account{}
+	logger := a.container.GetLogger()
+	if err != nil {
+		logger.GetZapLogger().Errorf(err.Error())
+		return false, nil
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(password)); err != nil {
+		logger.GetZapLogger().Errorf(err.Error())
+		return false, nil
+	}
+
+	return true, result
+}
+
+func (a *accountService) AuthenticateByUsername(username string) (bool, *model.Account) {
+	result, err := account.FindByName(rep, username)
+	rep := a.container.GetRepository()
+	logger := a.container.GetLogger()
+	account := model.Account{}
+	logger := a.container.GetLogger()
+	if err != nil {
+		logger.GetZapLogger().Errorf(err.Error())
+		return false, nil
+	}
+	return true, result
+}
+
+func (a *accountService) AuthenticateByUserIdAndPassword(userId string, password string) (bool, *model.Account) {
+	result, err := account.FindByUserId(rep, userId)
+	rep := a.container.GetRepository()
+	logger := a.container.GetLogger()
+	account := model.Account{}
+	logger := a.container.GetLogger()
+	if err != nil {
+		logger.GetZapLogger().Errorf(err.Error())
+		return false, nil
+	}
+
+	return true, result
+}
