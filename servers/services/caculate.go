0eae02c538a74205fb8475132f72ea0ff620538d
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-07-12 10:01:22 +0800

    修改

--- servers/services/caculate.go
+++ servers/services/caculate.go
@@ -131,3 +131,58 @@ func (s *checkInService) Count(cnd *sqls.Cnd) int64 {
 func (s *checkInService) GetByUserId(userId int64) *model.CheckIn {
 	return s.FindOne(sqls.NewCnd().Eq("user_id", userId))
 }
+
+func (s *checkInService) Updates(id int64, columns map[string]interface{}) error {
+	return repositories.CheckInRepository.Updates(sqls.DB(), id, columns)
+}
+
+
+func (s *checkInService) CheckInPoint(userId int64) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+	var (
+		checkIn         = s.GetByUserId(userId)
+		dayName         = dates.GetDay(time.Now())
+		yesterdayName   = dates.GetDay(time.Now().Add(-time.Hour * 24))
+		consecutiveDays = 1
+		err             error
+	)
+
+	if checkIn != nil && checkIn.LatestDayName == dayName {
+		return errors.New("你已签到")
+	}
+
+	if checkIn == nil {
+		err = s.Create(&model.CheckIn{
+			Model:           model.Model{},
+			UserId:          userId,
+			LatestDayName:   dayName,
+			ConsecutiveDays: consecutiveDays,
+			CreateTime:      dates.NowTimestamp(),
+			UpdateTime:      dates.NowTimestamp(),
+		})
+	} else {
+		checkIn.LatestDayName = dayName
+		checkIn.ConsecutiveDays = consecutiveDays
+		checkIn.UpdateTime = dates.NowTimestamp()
+		err = s.Update(checkIn)
+	}
+	
+	if checkIn != nil && checkIn.LatestDayName == yesterdayName {
+		consecutiveDays = checkIn.ConsecutiveDays + 1
+	}
+	
+	if err == nil {
+		// 清理签到排行榜缓存
+		cache.UserCache.RefreshCheckInRank()
+		// 处理签到积分
+		config := SysConfigService.GetConfig()
+		if config.ScoreConfig.CheckInScore > 0 {
+			_ = UserService.IncrScore(userId, config.ScoreConfig.CheckInScore, constants.EntityCheckIn,
+				strconv.FormatInt(userId, 10), "签到"+strconv.Itoa(dayName))
+		} else {
+			logrus.Warn("签到积分未配置...")
+		}
+	}
+	return err
+}
