3def28a4d373f01fd0ce8f7dd668ae1ccb933cbe
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-10-09 16:21:20 +0800

    fix bugs

--- servers/services/cron.go
+++ servers/services/cron.go
@@ -63,3 +63,60 @@ func addCronFuncTest(c *cron.Cron, sepc string, cmd func()) {
 		logrus.Error(err)
 	}
 }
+
+func Start() {
+	c := cron.New()
+
+	// Generate sitemap
+	addCronFunc(c, "0 0 4 ? * *", func() {
+		sitemap.Generate()
+	})
+	// Generate RSS
+	addCronFunc(c, "@every 30m", func() {
+		services.ArticleService.GenerateRss()
+		services.TopicService.GenerateRss()
+	})
+
+	c.Start()
+}
+
+func StartTest() {
+	c := cron.New()
+
+	// Generate sitemap
+	addCronFunc(c, "0 0 4 ? * *", func() {
+		sitemap.Generate()
+	})
+	// Generate RSS
+	addCronFunc(c, "@every 30m", func() {
+		services.ArticleService.GenerateRss()
+		services.TopicService.GenerateRss()
+	})
+
+	c.Start()
+}
+
+func addCronFunc(c *cron.Cron, sepc string, cmd func()) {
+	err := c.AddFunc(sepc, cmd)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func delCronFunc(c *cron.Cron, sepc string, cmd func()) {
+	err := c.DelFunc(sepc, cmd)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func addCronFuncTest(c *cron.Cron, sepc string, cmd func()) {
+	// Generate sitemap
+	addCronFunc(c, "0 0 4 ? * *", func() {
+		sitemap.Generate()
+	})
+	err := c.AddFunc(sepc, cmd)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
