8a9205f56c5046fbf91c4e1e290846c3fcef2314
happydonkey <TheGrandDuelist@users.noreply.github.com>
2024-12-02 14:10:46 +0800

    Create phone_verify_strategy.go

---
+++ servers/spam/phone_verify_strategy.go
@@ -0,0 +1,41 @@
+package spam
+
+import (
+	"bbs-go/model"
+	"bbs-go/pkg/errs"
+	"bbs-go/services"
+)
+
+type PhoneVerifyStrategy struct{}
+
+func (PhoneVerifyStrategy) Name() string {
+	return "PhoneVerifyStrategy"
+}
+
+func (PhoneVerifyStrategy) CheckTopic(user *model.User, form model.CreateTopicForm) error {
+	if services.SysConfigService.IsCreateTopicphoneVerified() && !user.phoneVerified {
+		return errs.EmailNotVerified
+	}
+	return nil
+}
+
+func (PhoneVerifyStrategy) CheckArticle(user *model.User, form model.CreateArticleForm) error {
+	if services.SysConfigService.IsCreateArticlephoneVerified() && !user.phoneVerified {
+		return errs.EmailNotVerified
+	}
+	return nil
+}
+
+func (PhoneVerifyStrategy) CheckComment(user *model.User, form model.CreateCommentForm) error {
+	if services.SysConfigService.IsCreateCommentphoneVerified() && !user.phoneVerified {
+		return errs.EmailNotVerified
+	}
+	return nil
+}
+
+func (PhoneVerifyStrategy) CheckTopicBak(user *model.User, form model.CreateTopicForm) error {
+	if services.SysConfigService.IsCreateTopicphoneVerified() && !user.phoneVerified {
+		return errs.EmailNotVerified
+	}
+	return nil
+}
