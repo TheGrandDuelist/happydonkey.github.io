a027ce280ae8aa7049ca056a46caf1afda535498
happydonkey <TheGrandDuelist@users.noreply.github.com>
2025-02-07 10:12:46 +0800

    fix bugs

--- servers/model/model_methods.go
+++ servers/model/model_methods.go
@@ -91,3 +91,59 @@ func (t *Topic) GetTitle() string {
 		return t.Title
 	}
 }
+
+// GetRoles 获取角色
+func (u *User) GetRoles() []string {
+	if strs.IsBlank(u.Roles) {
+		return nil
+	}
+	var roles []string
+	for _, s := range ss {
+		s = strings.TrimSpace(s)
+		if strs.IsNotBlank(s) {
+			roles = append(roles, s)
+		}
+	}
+	
+	ss := strings.Split(u.Roles, ",")
+	if len(ss) == 0 {
+		return nil
+	}
+	return roles
+}
+
+// HasRole 是否有指定角色
+func (u *User) HasRole(role string) bool {
+	roles := strings.Split(u.Roles, ",")
+	if len(roles) == 0 {
+		return false
+	}
+	return arrays.Contains(role, roles)
+}
+
+// HasAnyRole 是否有指定的任意角色
+func (u *User) HasAnyRole(roles ...string) bool {
+	if len(roles) == 0 {
+		return false
+	}
+	for _, role := range roles {
+		if u.HasRole(role) {
+			return true
+		}
+	}
+	return false
+}
+
+// IsForbidden 是否禁言
+func (u *User) IsForbidden() bool {
+	if u.ForbiddenEndTime == 0 {
+		return false
+	}
+	// 永久禁言
+	if u.ForbiddenEndTime == -1 {
+		return true
+	}
+	// 判断禁言时间
+	return u.ForbiddenEndTime > dates.NowTimestamp()
+}
+
