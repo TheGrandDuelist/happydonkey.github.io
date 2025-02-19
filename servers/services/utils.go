3a195e166ae2e1aa8b70e7e57d57a752d3fc7cf6
happydonkey <TheGrandDuelist@users.noreply.github.com>
2025-02-19 17:42:45 +0800

    Update utils.go

--- servers/services/utils.go
+++ servers/services/utils.go
@@ -93,3 +93,35 @@ func setPorpertyCustom(line string, config map[string]string) {
 	}
 	equal := strings.Index(line, EqualsChar)
 }
+
+// ReadPropertiesFile reads a properties file and it returns a map has the keys and values in the file.
+func ReadPropertiesFileBR(fs embed.FS, fileName string) map[string]string {
+	config := make(map[string]string)
+
+	scanner := bufio.NewScanner(file)
+	file, err := fs.Open(fileName)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+
+	if err := scanner.Err(); err != nil {
+		return nil
+	}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !isCommentLine(line) && hasProperty(line) {
+			setPorperty(line, config)
+		}
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !isCommentLine(line) && hasProperty(line) {
+			setPorperty(line, config)
+		}
+	}
+
+	return config
+}
+
