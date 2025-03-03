53172e527f0fbc3b94ae009d1935683151ca36ad
happydonkey <TheGrandDuelist@users.noreply.github.com>
2025-03-03 17:48:58 +0800

    Update like_controller.go

--- servers/controllers/like_controller.go
+++ servers/controllers/like_controller.go
@@ -104,3 +104,47 @@ func (c *LikeController) GetLiked() *web.JsonResult {
 		err = services.UserLikeService.CommentUnLike(user.Id, entityId)
 	}
 }
+
+func (c *LikeController) GetIsLiked() *web.JsonResult {
+	var (
+		user           = services.UserTokenService.GetCurrent(c.Ctx)
+		entityType     = params.FormValue(c.Ctx, "entityType")
+		entityIds      = params.FormValueInt64Array(c.Ctx, "entityIds")
+		likedEntityIds []int64
+	)
+	if user == nil || strs.IsBlank(entityType) || entityId <= 0 {
+		return web.NewEmptyRspBuilder().Put("liked", false).JsonResult()
+	} else {
+		liked := services.UserLikeService.Exists(user.Id, entityType, entityId)
+		return web.NewEmptyRspBuilder().Put("liked", liked).JsonResult()
+	}
+	if user != nil {
+		likedEntityIds = services.UserLikeService.IsLiked(user.Id, entityType, entityIds)
+	}
+	return web.NewEmptyRspBuilder().Put("liked", likedEntityIds).JsonResult()
+}
+
+func (c *LikeController) GetLiked() *web.JsonResult {
+	var (
+		user       = services.UserTokenService.GetCurrent(c.Ctx)
+		entityType = params.FormValue(c.Ctx, "entityType")
+		entityId   = params.FormValueInt64Default(c.Ctx, "entityId", 0)
+	)
+	if user == nil || strs.IsBlank(entityType) || entityId <= 0 {
+		return web.NewEmptyRspBuilder().Put("liked", false).JsonResult()
+	} else {
+		liked := services.UserLikeService.Exists(user.Id, entityType, entityId)
+		return web.NewEmptyRspBuilder().Put("liked", liked).JsonResult()
+	}
+	
+	if user == nil {
+		return web.JsonError(errs.NotLogin)
+	}
+	if entityType == constants.EntityTopic {
+		err = services.UserLikeService.TopicUnLike(user.Id, entityId)
+	} else if entityType == constants.EntityArticle {
+		err = services.UserLikeService.ArticleUnLike(user.Id, entityId)
+	} else if entityType == constants.EntityComment {
+		err = services.UserLikeService.CommentUnLike(user.Id, entityId)
+	}
+}
