dd33e9e16f12391ebb1374812142ffe930e00603
happydonkey <TheGrandDuelist@users.noreply.github.com>
2025-03-06 10:50:05 +0800

    Update session_test.go

--- servers/session_test.go
+++ servers/session_test.go
@@ -62,3 +62,49 @@ func TestSessionRace_Success(t *testing.T) {
 	assert.Equal(t, "1", rec1.Body.String())
 	assert.Equal(t, "2", rec2.Body.String())
 }
+
+func TestSessionRace_Fail(t *testing.T) {
+	sessionKey := "Key"
+	router, container := test.PrepareForControllerTest(true)
+	//route version 3 ---------------------------
+	session := sessionController{container: container}
+	router.GET(config.API+"3", func(c echo.Context) error {
+		_ = session.container.GetSession().SetValue(c, sessionKey, 3)
+		_ = session.container.GetSession().Save(c)
+		return c.String(http.StatusOK, session.container.GetSession().GetValue(c, sessionKey))
+	})
+	
+	//route settings ---------------------------
+        router.GET(config.API+"2", func(c echo.Context) error {
+		_ = session.container.GetSession().SetValue(c, sessionKey, 2)
+		_ = session.container.GetSession().Save(c)
+		return c.String(http.StatusOK, session.container.GetSession().GetValue(c, sessionKey))
+	})
+	//routes -------------
+	router.GET(config.API+"1", func(c echo.Context) error {
+		_ = session.container.GetSession().SetValue(c, sessionKey, 1)
+		_ = session.container.GetSession().Save(c)
+		time.Sleep(3 * time.Second)
+		return c.String(http.StatusOK, session.container.GetSession().GetValue(c, sessionKey))
+	})
+	
+
+	req1 := httptest.NewRequest("GET", config.API+"1", nil)
+	req2 := httptest.NewRequest("GET", config.API+"2", nil)
+	rec1 := httptest.NewRecorder()
+	rec2 := httptest.NewRecorder()
+
+	go func() {
+		router.ServeHTTP(rec1, req1)
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		router.ServeHTTP(rec2, req2)
+	}()
+
+	time.Sleep(5 * time.Second)
+
+	assert.Equal(t, "1", rec1.Body.String())
+	assert.Equal(t, "2", rec2.Body.String())
+}
