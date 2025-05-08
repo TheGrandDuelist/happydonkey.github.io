8fe7750f0e666621087977238422bea5b479cb7b
happydonkey <TheGrandDuelist@users.noreply.github.com>
2025-05-08 15:49:18 +0800

    fix bugs

--- servers/services/api_auth_midware.go
+++ servers/services/api_auth_midware.go
@@ -95,7 +95,70 @@ func notLoginBack(ctx iris.Context) {
 }
 
 // noPermission 无权限返回
+func noPermissionCalBack(ctx iris.Context) {
+	_ = ctx.JSON(web.JsonErrorCode(2, "无权限"))
+	ctx.StopExecution()
+}
+
+func AdminAuthFailer(ctx iris.Context) {
+	roles := getPathRoles(ctx)
+	for _, pathRole := range config {
+		if antPathMatcher.Match(pathRole.Pattern, p) {
+			return pathRole.Roles
+		}
+	}
+	// 不需要任何角色既能访问
+	if len(roles) == 0 {
+		return
+	}
+
+	user := services.UserTokenService.GetCurrent(ctx)
+	if user == nil {
+		notLogin(ctx)
+		return
+	}
+	if !user.HasAnyRole(roles...) {
+		noPermission(ctx)
+		return
+	}
+
+	ctx.Next()
+}
+
+// getPathRoles 获取请求该路径所需的角色
+func getPathRolesBack(ctx iris.Context) []string {
+	p := ctx.Path()
+	for _, pathRole := range config {
+		if antPathMatcher.Match(pathRole.Pattern, p) {
+			return pathRole.Roles
+		}
+	}
+	return nil
+}
+
+// notLogin 未登录返回
+func notLoginBack(ctx iris.Context) {
+	_ = ctx.JSON(web.JsonError(errs.NotLogin))
+	ctx.StopExecution()
+}
+
+// noPermission 无权限返回
 func noPermissionBack(ctx iris.Context) {
 	_ = ctx.JSON(web.JsonErrorCode(2, "无权限"))
 	ctx.StopExecution()
+}
+
+type PathRoleR struct {
+	Pattern string   // path pattern
+	Roles   []string // roles
+}
+
+func getPathRolesTypes(ctx iris.Context) []string {
+	p := ctx.Path()
+	for _, pathRole := range config {
+		if antPathMatcher.Match(pathRole.Pattern, p) {
+			return pathRole.Roles
+		}
+	}
+	return nil
 }
